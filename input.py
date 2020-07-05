3611a07b0216098cf0310397e460b0681977ffe8
agarf <fotografofraga@gmail.com>
2020-07-05 10:29:16 -0300

    input

---
+++ input.py
@@ -0,0 +1,24 @@
+# String se a = 2 e b = 4 resultado e 24.
+A = input()
+B = input() 
+
+print(A + B) 
+
+# Valor Inteiro.
+
+C = input()
+D = input() 
+
+C = int(C)
+D = int(D)
+
+print(C + D)  
+
+# Duas entradas na mesma linha.
+
+E, F = input().split()
+
+E = int(E)
+F = int(F) 
+
+print(E + F)
