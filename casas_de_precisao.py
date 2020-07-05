8074744c883e49fef6870ea350a97a4e22ebf6af
agarf <fotografofraga@gmail.com>
2020-07-05 10:27:31 -0300

    casas de precisao

---
+++ casas_de_precisao.py
@@ -0,0 +1,17 @@
+A = 4
+B = 3
+
+# printf sem casa de precisao
+print("resultado da divisao: {}".format(A / B)) 
+
+# com duas casas de precisao.
+print("resultado da divisao: {:.2f}".format(A / B))
+
+# com tres casas de precisao.
+print("resultado da divisao: {:.3f}".format(A / B)) 
+
+# Nenhuma casa de precisao.Somente a parte inteira.
+print("resultado da divisao: {:.0f}".format(A / B))
+
+print("Neps")
+print("Academy")
