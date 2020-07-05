b71bac7b61346ab069ac68af10236642ef8ae116
agarf <fotografofraga@gmail.com>
2020-07-05 10:28:24 -0300

    estruturas de dados

---
+++ estruturas_decisao.py
@@ -0,0 +1,16 @@
+
+# Estrutura de decisao.
+
+A = input()
+
+A = float(A)
+
+if A >= 7:
+    print("Aluno Aprovado")
+elif A >= 5:
+    print("Aluno de recuperacao")
+
+else:
+    print("Aluno reprovado")
+
+print("Final do semestre \0/")
